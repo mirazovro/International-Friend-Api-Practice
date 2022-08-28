a182d942bc12a436f92d3ce93fd8cb273930ac6a
unknown <mirazovro@gmail.com>
2022-08-28 16:07:34 +0600

    Country-name-and-flag-added

--- js/countries.js
+++ js/countries.js
@@ -21,8 +21,30 @@ const displayCountries = (countries) => {
       <h2>Borders:${
         country.borders ? country.borders[0] : "Not any borders"
       }</h2>
+      <button onClick="loadCountryDetail('${country.cca2}')"> Details</button>
+
       `;
     countryContainer.appendChild(countryDIv);
   });
 };
+
+const loadCountryDetail = (code) => {
+  //https://restcountries.com/v3.1/alpha/{code}
+  const url = `https://restcountries.com/v3.1/alpha/${code}`;
+  //   console.log("get country details", code);
+  fetch(url)
+    .then((res) => res.json())
+    .then((Data) => displyCountryDetail(Data[0]));
+};
+
+const displyCountryDetail = (country) => {
+  //   console.log(country);
+
+  const CountryDetail = document.getElementById("country-details");
+  CountryDetail.innerHTML = `
+    <h2>Name:${country.name.common}</h2>
+    <img src="${country.flags.png}"></img>
+    `;
+};
+
 loadCountries();
