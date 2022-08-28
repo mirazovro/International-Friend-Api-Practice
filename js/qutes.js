15eadd53e3d3e0aa8a8513ed6ecc1ae1547e2492
unknown <mirazovro@gmail.com>
2022-08-28 11:41:40 +0600

    random-user-and-bedeshi-api-complet

---
+++ js/qutes.js
@@ -0,0 +1,14 @@
+const loadQutes = () => {
+  fetch("");
+  fetch("https://api.kanye.rest/")
+    .then((res) => res.json())
+    .then((data) => loadData(data));
+};
+
+const loadData = (loadedData) => {
+  //   console.log(loadedData);
+  const blockQutes = document.getElementById("quote");
+  blockQutes.innerText = loadedData.quote;
+};
+
+loadQutes();
---
+++ js/randomuser.js
@@ -0,0 +1,28 @@
+const Loaduser = () => {
+  fetch("https://randomuser.me/api/?results=50")
+    .then((res) => res.json())
+    .then((data) => displayUsers(data.results));
+};
+
+const displayUsers = (users) => {
+  const userContainer = document.getElementById("user-container");
+  for (const user of users) {
+    console.log(user);
+    const userDiv = document.createElement("div");
+    userDiv.classList.add("user");
+    userDiv.innerHTML = `
+      <h3>User Name:${user.name.title} ${user.name.first} ${user.name.last}</h3>
+      <p>Email: ${user.email}</p>
+      </p>User location:${user.location.city}${user.location.country}</P>
+      <p>User CellPhone: ${user.cell} </p>
+      <p>User Id: ${user.id.value} </p>
+      <p>User Login: ${user.login.uuid} </p>
+      <img src=" ${user.picture.large}">
+      <p>User Id: ${user.id.name} </p>
+      
+      `;
+    userContainer.appendChild(userDiv);
+  }
+};
+
+Loaduser();
