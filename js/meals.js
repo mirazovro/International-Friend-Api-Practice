bbc97150546dcd214590945696b02bb0cb47f207
unknown <mirazovro@gmail.com>
2022-08-28 17:34:00 +0600

    meals-added

---
+++ js/meals.js
@@ -0,0 +1,30 @@
+const loadMeals = () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+};
+
+const displayMeals = (meals) => {
+  const mealsContainer = document.getElementById("meal-container");
+  meals.forEach((meal) => {
+    // console.log(meal);
+
+    const mealDiv = document.createElement("div");
+    mealDiv.classList.add("col");
+
+    mealDiv.innerHTML = `<div class="card">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+          to additional content.</p>
+  </div>
+</div>
+    
+    `;
+
+    mealsContainer.appendChild(mealDiv);
+  });
+};
+
+loadMeals();
